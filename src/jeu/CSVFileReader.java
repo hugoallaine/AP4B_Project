00372c7745bd7f517126d34206d7dc33dd8c1bc0
HugoPlayer <hugo.allaine@gmail.com>
2024-01-08 11:01:34 +0100

    Ajout Doxygen documentation

--- src/jeu/CSVFileReader.java
+++ src/jeu/CSVFileReader.java
@@ -6,7 +6,16 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @brief Classe permettant de lire un fichier CSV
+ */
 public class CSVFileReader {
+
+    /**
+     * @brief Méthode permettant de lire un fichier CSV
+     * @param filePath Chemin du fichier CSV
+     * @return Liste de String[] contenant les données du fichier CSV
+     */
     public static List<String[]> readCSV(String filePath){
         if(filePath == null) throw new IllegalArgumentException("[ERROR] The path given is null!");
         if(!filePath.split("\\.")[1].equals("csv")) throw new IllegalArgumentException("[ERROR] The file must be a CSV file!");
@@ -21,11 +30,9 @@ public class CSVFileReader {
                 }
                 Card_Data.add(ligne_split);
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        return Card_Data;
-        
-    }
-    
+        return Card_Data;   
+    }   
 }
