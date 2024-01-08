3e48a2861af2abca279a083a98eebe4cbe7df299
HugoPlayer <hugo.allaine@gmail.com>
2024-01-08 10:41:16 +0100

    [TreasureCard.java] Ajout Doxygen documentation

--- src/jeu/Cards/TreasureCard.java
+++ src/jeu/Cards/TreasureCard.java
@@ -1,10 +1,18 @@
 package src.jeu.Cards;
 
+/**
+ * @brief Classe abstraite des cartes trésor
+ * @details Les cartes trésors sont des cartes qui peuvent être gagnées par le joueur
+ */
 public abstract class TreasureCard extends Card{
+
+    /**
+     * @brief Constructeur de la classe TreasureCard
+     * @param name Le nom de la carte
+     * @param desc La description de la carte
+     * @param targetMode Le mode de ciblage de la carte
+     */
     protected TreasureCard(String name, String desc, CardTargetMode targetMode){
         super(name, desc, targetMode);
     }   
-
-    
 }
-
