fcd6128cc2e80971fed22fe47fcb4b1299c5c5b0
HugoPlayer <hugo.allaine@gmail.com>
2024-01-08 10:45:07 +0100

    [Exceptions] Ajout Doxygen documentation

--- src/jeu/Exceptions/InvalidPlayerNameException.java
+++ src/jeu/Exceptions/InvalidPlayerNameException.java
@@ -1,5 +1,8 @@
 package src.jeu.Exceptions;
 
+/**
+ * @brief Classe d'exception pour les noms de joueurs invalides
+ */
 public class InvalidPlayerNameException extends Exception{
     public InvalidPlayerNameException(){
         super();
